a0146da72f2bf0163ee8a66ad712a59c8b299661
gokul-1998 <gokulakrishnanm1998@gmail.com>
2025-04-24 15:22:40 +0530

    added size

--- scrape_flow_app/components/ui/button.tsx
+++ scrape_flow_app/components/ui/button.tsx
@@ -21,6 +21,12 @@ const buttonVariants = cva(
         sidebarItem:"gap-2 !justify-start hover:bg-accent hover:bg-primary/80 hover:text-primary-foreground",
         sidebarActiveItem:"gap-2 !justify-start bg-primary text-primary-foreground hover:bg-primary/90",
       },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3 text-sm",
+        lg: "h-11 rounded-md px-8 text-lg",
+        icon: "h-10 w-10",
+      },
     },
     defaultVariants: {
       variant: "default",
