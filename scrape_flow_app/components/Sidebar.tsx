ab06d13e1d068ea77da2532c4c4a5cce3fdefa05
gokul-1998 <gokulakrishnanm1998@gmail.com>
2025-04-24 13:49:01 +0530

    19.27 - small bug in sidebar icons

--- scrape_flow_app/components/Sidebar.tsx
+++ scrape_flow_app/components/Sidebar.tsx
@@ -4,6 +4,8 @@ import { CoinsIcon, HomeIcon, icons, Layers2Icon, ShieldCheckIcon } from 'lucide
 import React from 'react'
 import Logo from './Logo'
 import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+import { usePathname } from 'next/navigation'
 
 const routes =[
     {
@@ -29,6 +31,8 @@ const routes =[
 ]
 
 function DesktopSidebar() {
+    const pathname=usePathname();
+    const activeRoute=routes.find((route) => route.href.length>0 && pathname.includes(route.href))|| routes[0];
   return (
     <div className='hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate'>
         <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
@@ -36,7 +40,9 @@ function DesktopSidebar() {
         </div>
         <div className="flex flex-col p-2">
             {routes.map((route) => (
-                <Link key={route.href} href={route.href}>
+                <Link key={route.href} href={route.href}
+                    className={buttonVariants({variant:activeRoute.href===route.href?"sidebarActiveItem":"sidebarItem"} )}
+                    >
                     <route.icon size={20} />
                     {route.label}
                 </Link>
--- scrape_flow_app/components/ui/button.tsx
+++ scrape_flow_app/components/ui/button.tsx
@@ -18,12 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        sidebarItem:"gap-2 !justify-start hover:bg-accent hover:bg-primary/80 hover:text-primary-foreground",
+        sidebarActiveItem:"gap-2 !justify-start bg-primary text-primary-foreground hover:bg-primary/90",
       },
     },
     defaultVariants: {
@@ -31,7 +27,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+) 
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
