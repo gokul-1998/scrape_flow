851709b38c60f6309c8155ca2c40f2f45824beb3
gokul-1998 <gokulakrishnanm1998@gmail.com>
2025-04-24 13:35:35 +0530

    16.56

--- scrape_flow_app/components/Sidebar.tsx
+++ scrape_flow_app/components/Sidebar.tsx
@@ -3,27 +3,28 @@
 import { CoinsIcon, HomeIcon, icons, Layers2Icon, ShieldCheckIcon } from 'lucide-react'
 import React from 'react'
 import Logo from './Logo'
+import Link from 'next/link'
 
 const routes =[
     {
         href:"",
         label:"Home",
-        icons:HomeIcon
+        icon:HomeIcon
     },
     {
         href:"workflows",
         label:"Workflows",
-        icons:Layers2Icon
+        icon:Layers2Icon
     },
     {
         href:"credentials",
         label:"Credentials",
-        icons:ShieldCheckIcon,
+        icon:ShieldCheckIcon,
     },
     {
         href:"billing",
         label:"Billing",
-        icons:CoinsIcon
+        icon:CoinsIcon
     }
 ]
 
@@ -32,6 +33,14 @@ function DesktopSidebar() {
     <div className='hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate'>
         <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
             <Logo/>
+        </div>
+        <div className="flex flex-col p-2">
+            {routes.map((route) => (
+                <Link key={route.href} href={route.href}>
+                    <route.icon size={20} />
+                    {route.label}
+                </Link>
+            ))}
         </div>
 
 
