c3cde55f16ed6ffc1bb97a8a77fee4abe3c5dde5
gokul-1998 <gokulakrishnanm1998@gmail.com>
2025-04-24 13:07:02 +0530

    12.45

---
+++ scrape_flow_app/components/Sidebar.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { CoinsIcon, HomeIcon, icons, Layers2Icon, ShieldCheckIcon } from 'lucide-react'
+import React from 'react'
+
+const routes =[
+    {
+        href:"",
+        label:"Home",
+        icons:HomeIcon
+    },
+    {
+        href:"workflows",
+        label:"Workflows",
+        icons:Layers2Icon
+    },
+    {
+        href:"credentials",
+        label:"Credentials",
+        icons:ShieldCheckIcon,
+    },
+    {
+        href:"billing",
+        label:"Billing",
+        icons:CoinsIcon
+    }
+]
+
+function DesktopSidebar() {
+  return (
+    <div className='hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate'>
+         Desktop Sidebar
+    </div>
+  )
+}
+
+export default DesktopSidebar
